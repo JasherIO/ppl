696c75970b5c5e3d35d092ac30a934dd8ba23bde
Willis <3475947+JasherIO@users.noreply.github.com>
2018-09-23 09:32:57 -0500

    Navbar burger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,63 +2,160 @@ import React from 'react'
 import { Container,
          Navbar, 
          NavbarBrand,
+         NavbarBurger,
          NavbarDropdown,
-         NavbarItem } from 'bloomer'
+         NavbarEnd,
+         NavbarItem,
+         NavbarMenu,
+         NavbarStart } from 'bloomer'
 import { Link } from 'gatsby'
 
-const Nav = () => (
-  <Navbar isTransparent className='is-dark'>
-    <Container isFluid>
-      <NavbarBrand>
-        <Link to='/' className='navbar-item'>
-          <img src="/img/white-transparent.png" alt="Logo"></img>
-        </Link>
-        <Link to="/news" className='navbar-item' activeClassName="is-active">
-          News
-        </Link>
+class Nav extends React.Component {
+  constructor(props) { 
+    super(props)
+    
+    this.state = { isActive: false }
+    this.onClickNav = this.onClickNav.bind(this)
+  }
+
+  onClickNav() {
+    this.setState({ isActive: !this.state.isActive})
+  }
+
+  render() {
+    return (
+      <Navbar isTransparent className='is-dark'>
+        <Container isFluid>
+          <NavbarBrand>
+            <Link to='/' className='navbar-item'>
+              <img src="/img/white-transparent.png" alt="Logo"></img>
+            </Link>
+            
+
+            {/* <NavbarItem hasDropdown isHoverable>
+              <NavbarLink href='/division1'>Division 1</NavbarLink>
+              <NavbarDropdown>
+                <NavbarItem href='/division1/stats'>Stats</NavbarItem>
+                <NavbarItem href='/division1/standings'>Standings</NavbarItem>
+              </NavbarDropdown>
+            </NavbarItem> */}
+
+            {/* <NavbarItem hasDropdown isHoverable>
+              <NavbarLink href='/division2'>Division 2</NavbarLink>
+              <NavbarDropdown>
+                <NavbarItem href='/division2/stats'>Stats</NavbarItem>
+                <NavbarItem href='/division2/standings'>Standings</NavbarItem>
+              </NavbarDropdown>
+            </NavbarItem> */}
+
+            {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
+              <FontAwesomeIcon icon={['fab', 'github']} />
+            </NavbarItem> */}
+            {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
+              <FontAwesomeIcon icon={['fab', 'twitter']} />
+            </NavbarItem> */}
+
+            <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+          </NavbarBrand>
+
+          <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+            <NavbarStart>
+              <Link to="/news" className='navbar-item' activeClassName="is-active">
+                News
+              </Link>
+              
+              <NavbarItem hasDropdown isHoverable>
+                {/* <NavbarLink href='/premier'>Premier</NavbarLink> */}
+                <Link to="/premier" className='navbar-link' activeClassName="is-active">
+                  Premier
+                </Link>
+                <NavbarDropdown isBoxed>
+                  {/* <NavbarItem href='/premier/stats'>Stats</NavbarItem> */}
+                  {/* <NavbarItem href='/premier/standings'>Standings</NavbarItem> */}
+                  <Link to="/premier/standings" className='navbar-item' activeClassName="is-active">
+                    Standings
+                  </Link>
+                  <Link to="/premier/stats" className='navbar-item' activeClassName="is-active">
+                    Stats
+                  </Link>
+                </NavbarDropdown>
+              </NavbarItem>
+
+            </NavbarStart>
+            <NavbarEnd>
+              {/* <NavbarItem href='#/'>Home</NavbarItem> */}
+            </NavbarEnd>
+          </NavbarMenu>
+        </Container>
+      </Navbar>
+    )
+  }
+}
+
+// const Nav = () => (
+//   <Navbar isTransparent className='is-dark'>
+//     <Container isFluid>
+//       <NavbarBrand>
+//         <Link to='/' className='navbar-item'>
+//           <img src="/img/white-transparent.png" alt="Logo"></img>
+//         </Link>
+//         <Link to="/news" className='navbar-item' activeClassName="is-active">
+//           News
+//         </Link>
         
-        <NavbarItem hasDropdown isHoverable>
-          {/* <NavbarLink href='/premier'>Premier</NavbarLink> */}
-          <Link to="/premier" className='navbar-link' activeClassName="is-active">
-            Premier
-          </Link>
-          <NavbarDropdown isBoxed>
-            {/* <NavbarItem href='/premier/stats'>Stats</NavbarItem> */}
-            {/* <NavbarItem href='/premier/standings'>Standings</NavbarItem> */}
-            <Link to="/premier/standings" className='navbar-item' activeClassName="is-active">
-              Standings
-            </Link>
-            <Link to="/premier/stats" className='navbar-item' activeClassName="is-active">
-              Stats
-            </Link>
-          </NavbarDropdown>
-        </NavbarItem>
+//         <NavbarItem hasDropdown isHoverable isHidden='touch'>
+//           {/* <NavbarLink href='/premier'>Premier</NavbarLink> */}
+//           <Link to="/premier" className='navbar-link' activeClassName="is-active">
+//             Premier
+//           </Link>
+//           <NavbarDropdown isBoxed>
+//             {/* <NavbarItem href='/premier/stats'>Stats</NavbarItem> */}
+//             {/* <NavbarItem href='/premier/standings'>Standings</NavbarItem> */}
+//             <Link to="/premier/standings" className='navbar-item' activeClassName="is-active">
+//               Standings
+//             </Link>
+//             <Link to="/premier/stats" className='navbar-item' activeClassName="is-active">
+//               Stats
+//             </Link>
+//           </NavbarDropdown>
+//         </NavbarItem>
 
-        {/* <NavbarItem hasDropdown isHoverable>
-          <NavbarLink href='/division1'>Division 1</NavbarLink>
-          <NavbarDropdown>
-            <NavbarItem href='/division1/stats'>Stats</NavbarItem>
-            <NavbarItem href='/division1/standings'>Standings</NavbarItem>
-          </NavbarDropdown>
-        </NavbarItem> */}
+//         {/* <NavbarItem hasDropdown isHoverable>
+//           <NavbarLink href='/division1'>Division 1</NavbarLink>
+//           <NavbarDropdown>
+//             <NavbarItem href='/division1/stats'>Stats</NavbarItem>
+//             <NavbarItem href='/division1/standings'>Standings</NavbarItem>
+//           </NavbarDropdown>
+//         </NavbarItem> */}
 
-        {/* <NavbarItem hasDropdown isHoverable>
-          <NavbarLink href='/division2'>Division 2</NavbarLink>
-          <NavbarDropdown>
-            <NavbarItem href='/division2/stats'>Stats</NavbarItem>
-            <NavbarItem href='/division2/standings'>Standings</NavbarItem>
-          </NavbarDropdown>
-        </NavbarItem> */}
+//         {/* <NavbarItem hasDropdown isHoverable>
+//           <NavbarLink href='/division2'>Division 2</NavbarLink>
+//           <NavbarDropdown>
+//             <NavbarItem href='/division2/stats'>Stats</NavbarItem>
+//             <NavbarItem href='/division2/standings'>Standings</NavbarItem>
+//           </NavbarDropdown>
+//         </NavbarItem> */}
 
-        {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
-          <FontAwesomeIcon icon={['fab', 'github']} />
-        </NavbarItem> */}
-        {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
-          <FontAwesomeIcon icon={['fab', 'twitter']} />
-        </NavbarItem> */}
-      </NavbarBrand>
-    </Container>
-  </Navbar>
-)
+//         {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
+//           <FontAwesomeIcon icon={['fab', 'github']} />
+//         </NavbarItem> */}
+//         {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
+//           <FontAwesomeIcon icon={['fab', 'twitter']} />
+//         </NavbarItem> */}
+
+//         <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+//       </NavbarBrand>
+
+//       <NavbarMenu isActive={this.state.isActive} onClick={this.onClick}>
+//         <NavbarStart>
+//           <NavbarItem href='#/'>Home</NavbarItem>
+//         </NavbarStart>
+//         <NavbarEnd>
+//           <NavbarItem href='#/'>Home</NavbarItem>
+//         </NavbarEnd>
+//       </NavbarMenu>
+//     </Container>
+//   </Navbar>
+// )
 
 export default Nav
