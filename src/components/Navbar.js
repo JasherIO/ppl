0a86eeb2ec83a845e012f7f16db7814555925cd2
Willis <3475947+JasherIO@users.noreply.github.com>
2018-09-24 10:37:04 -0500

    Cleanup / removed unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,23 +31,6 @@ class Nav extends React.Component {
             <Link to='/' className='navbar-item'>
               <img src="/img/white-transparent.png" alt="Logo"></img>
             </Link>
-            
-
-            {/* <NavbarItem hasDropdown isHoverable>
-              <NavbarLink href='/division1'>Division 1</NavbarLink>
-              <NavbarDropdown>
-                <NavbarItem href='/division1/stats'>Stats</NavbarItem>
-                <NavbarItem href='/division1/standings'>Standings</NavbarItem>
-              </NavbarDropdown>
-            </NavbarItem> */}
-
-            {/* <NavbarItem hasDropdown isHoverable>
-              <NavbarLink href='/division2'>Division 2</NavbarLink>
-              <NavbarDropdown>
-                <NavbarItem href='/division2/stats'>Stats</NavbarItem>
-                <NavbarItem href='/division2/standings'>Standings</NavbarItem>
-              </NavbarDropdown>
-            </NavbarItem> */}
 
             <NavbarItem href='https://discord.gg/fV4VrQ4' isHidden='desktop'>
               <FontAwesomeIcon icon={['fab', 'discord']} />
@@ -57,7 +40,7 @@ class Nav extends React.Component {
               <FontAwesomeIcon icon={['fab', 'twitch']} />
             </NavbarItem>
             
-            <NavbarItem href='https://twitter.com/pulsarpremierleague' isHidden='desktop'>
+            <NavbarItem href='https://twitter.com/pulsarpremier' isHidden='desktop'>
               <FontAwesomeIcon icon={['fab', 'twitter']} />
             </NavbarItem>
 
@@ -71,13 +54,10 @@ class Nav extends React.Component {
               </Link>
               
               <NavbarItem hasDropdown isHoverable>
-                {/* <NavbarLink href='/premier'>Premier</NavbarLink> */}
                 <Link to="/premier" className='navbar-link' activeClassName="is-active">
                   Premier
                 </Link>
                 <NavbarDropdown isBoxed>
-                  {/* <NavbarItem href='/premier/stats'>Stats</NavbarItem> */}
-                  {/* <NavbarItem href='/premier/standings'>Standings</NavbarItem> */}
                   <Link to="/premier/standings" className='navbar-item' activeClassName="is-active">
                     Standings
                   </Link>
@@ -107,71 +87,5 @@ class Nav extends React.Component {
     )
   }
 }
-
-// const Nav = () => (
-//   <Navbar isTransparent className='is-dark'>
-//     <Container isFluid>
-//       <NavbarBrand>
-//         <Link to='/' className='navbar-item'>
-//           <img src="/img/white-transparent.png" alt="Logo"></img>
-//         </Link>
-//         <Link to="/news" className='navbar-item' activeClassName="is-active">
-//           News
-//         </Link>
-        
-//         <NavbarItem hasDropdown isHoverable isHidden='touch'>
-//           {/* <NavbarLink href='/premier'>Premier</NavbarLink> */}
-//           <Link to="/premier" className='navbar-link' activeClassName="is-active">
-//             Premier
-//           </Link>
-//           <NavbarDropdown isBoxed>
-//             {/* <NavbarItem href='/premier/stats'>Stats</NavbarItem> */}
-//             {/* <NavbarItem href='/premier/standings'>Standings</NavbarItem> */}
-//             <Link to="/premier/standings" className='navbar-item' activeClassName="is-active">
-//               Standings
-//             </Link>
-//             <Link to="/premier/stats" className='navbar-item' activeClassName="is-active">
-//               Stats
-//             </Link>
-//           </NavbarDropdown>
-//         </NavbarItem>
-
-//         {/* <NavbarItem hasDropdown isHoverable>
-//           <NavbarLink href='/division1'>Division 1</NavbarLink>
-//           <NavbarDropdown>
-//             <NavbarItem href='/division1/stats'>Stats</NavbarItem>
-//             <NavbarItem href='/division1/standings'>Standings</NavbarItem>
-//           </NavbarDropdown>
-//         </NavbarItem> */}
-
-//         {/* <NavbarItem hasDropdown isHoverable>
-//           <NavbarLink href='/division2'>Division 2</NavbarLink>
-//           <NavbarDropdown>
-//             <NavbarItem href='/division2/stats'>Stats</NavbarItem>
-//             <NavbarItem href='/division2/standings'>Standings</NavbarItem>
-//           </NavbarDropdown>
-//         </NavbarItem> */}
-
-//         {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
-//           <FontAwesomeIcon icon={['fab', 'github']} />
-//         </NavbarItem> */}
-//         {/* <NavbarItem href='https://twitter.com/pulsarpremierleague'>
-//           <FontAwesomeIcon icon={['fab', 'twitter']} />
-//         </NavbarItem> */}
-
-//         <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
-//       </NavbarBrand>
-
-//       <NavbarMenu isActive={this.state.isActive} onClick={this.onClick}>
-//         <NavbarStart>
-//           <NavbarItem href='#/'>Home</NavbarItem>
-//         </NavbarStart>
-//         <NavbarEnd>
-//           <NavbarItem href='#/'>Home</NavbarItem>
-//         </NavbarEnd>
-//       </NavbarMenu>
-//     </Container>
-//   </Navbar>
-// )
 
 export default Nav
