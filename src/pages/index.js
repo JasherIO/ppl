c60e0d1811e33dd868d889b690c42e47ec90472f
Willis <3475947+JasherIO@users.noreply.github.com>
2018-10-05 10:28:18 -0500

    List news cards; news level

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'gatsby'
+import moment from 'moment'
+
+const posts = [
+  {
+    'id': 1,
+    'img': '/img/Batmobile.jpg',
+    'title': 'Season 4: Week 1 In Review',
+    'category': 'Review',
+    'date': '2018-10-04 09:30:00+07:00'
+  },
+  {
+    'id': 2,
+    'img': '/img/XDevil.jpg',
+    'title': 'Week 1 Schedule Released',
+    'category': 'News',
+    'date': '2018-10-01 09:30:00+07:00'
+  },
+  {
+    'id': 3,
+    'img': '/img/GrandChampion.png',
+    'title': 'Season 4 Kicks Off December 1st!',
+    'category': 'News',
+    'date': '2018-09-25 09:30:00+07:00'
+  }
+]
 
 export default class IndexPage extends React.Component {
   render() {
@@ -20,67 +46,44 @@ export default class IndexPage extends React.Component {
         
         <section className="section">
           <div className="container">
+            <div className="level">
+              <div className="level-left">
+                <div className="level-item">
+                  <p className="title is-4">Recent News</p>
+                </div>
+              </div>
+              <div className="level-right">
+                <div className="level-item">
+                  <Link to="/news" className="button is-primary is-outlined">
+                    View All News
+                  </Link>
+                </div>
+              </div>
+            </div>
+
             <div className="columns">
 
-              <div className="column">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image is-16by9">
-                      <img src="/img/Batmobile.jpg" alt="Placeholder image" />
-                    </figure>
-                  </div>
-                  <div className="card-content">
-                    <div className="content">
-                      <p className="title is-4">Season 4: Week 1 In Review</p>
-                      <p className="subtitle is-6">
-                        <span className="has-text-primary is-uppercase has-text-weight-semibold">Review</span>
-                        &nbsp;
-                        <time datetime="2018-10-04" className="has-text-weight-light">a day ago</time>
-                      </p>
+              {posts.map((post) => (
+                <div className="column" key={post.id}>
+                  <div className="card">
+                    <div className="card-image">
+                      <figure className="image is-16by9">
+                        <img src={post.img} />
+                      </figure>
+                    </div>
+                    <div className="card-content">
+                      <div className="content">
+                        <p className="title is-4">{post.title}</p>
+                        <p className="subtitle is-6">
+                          <span className="has-text-primary is-uppercase has-text-weight-semibold">{post.category}</span>
+                          &nbsp;
+                          <time dateTime={post.date} className="has-text-weight-light">{moment(post.date).fromNow()}</time>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="column">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image is-16by9">
-                      <img src="/img/XDevil.jpg" alt="Placeholder image" />
-                    </figure>
-                  </div>
-                  <div className="card-content">
-                    <div className="content">
-                      <p className="title is-4">Week 1 Schedule Released</p>
-                      <p className="subtitle is-6">
-                        <span className="has-text-primary is-uppercase has-text-weight-semibold">News</span>
-                        &nbsp;
-                        <time datetime="2018-10-04" className="has-text-weight-light">6 days ago</time>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="column">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image is-16by9">
-                      <img src="/img/GrandChampion.png" alt="Placeholder image" />
-                    </figure>
-                  </div>
-                  <div className="card-content">
-                    <div className="content">
-                      <p className="title is-4">Season 4 Kicks Off December 1st!</p>
-                      <p className="subtitle is-6">
-                        <span className="has-text-primary is-uppercase has-text-weight-semibold">News</span>
-                        &nbsp;
-                        <time datetime="2018-10-04" className="has-text-weight-light">09/26/2018</time>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
 
             </div>
           </div>
