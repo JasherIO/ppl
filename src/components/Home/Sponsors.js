7da076909a22e822f07564cc109082625454edef
Willis <3475947+JasherIO@users.noreply.github.com>
2018-10-06 16:50:51 -0500

    Sponsor section

--- src/components/Home/Sponsors.js
+++ src/components/Home/Sponsors.js
@@ -5,8 +5,48 @@ import React from 'react'
 const Section = () => {
   return (
     <section className="section">
-      <div className="container">
-        Sponsors go here
+      <div className="container is-narrow">
+        <p className="title is-4">Sponsored By</p>
+
+        <div className="columns">
+          
+          <div className="column is-one-third">
+            <figure className="image is-1b1">
+              <img src='/img/AllYours.png' alt='All Yours Buffet' />          
+              <figcaption className="has-text-centered">
+                <p className="title is-5">
+                  All the colored eggs you could ever want!
+                </p>
+                <a href="https://google.com" class="button is-primary is-outlined">Visit Website</a>
+              </figcaption>
+            </figure>
+          </div>
+
+          <div className="column is-one-third">
+            <figure className="image is-1b1">
+              <img src='/img/WhatASave.png' alt='What a Save Esports' />          
+              <figcaption className="has-text-centered">
+                <p className="title is-5">
+                  The cost cutting esports organization saving the world!
+                </p>
+                <a href="https://google.com" class="button is-primary is-outlined">Visit Website</a>
+              </figcaption>
+            </figure>
+          </div>
+
+          <div className="column is-one-third">
+            <figure className="image is-1b1">
+              <img src='/img/EggingGrocers.png' alt='Egging Grocers' />          
+              <figcaption className="has-text-centered">
+                <p className="title is-5">
+                  Egg Grocers! Just do it! #JorbyApproved
+                </p>
+                <a href="https://google.com" class="button is-primary is-outlined">Visit Website</a>
+              </figcaption>
+            </figure>
+          </div>
+
+        </div>
       </div>
     </section>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 import Hero from '../components/Home/Hero'
 import News from '../components/Home/News'
 // import About from '../components/Home/About'
+import Sponsors from '../components/Home/Sponsors'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class IndexPage extends React.Component {
         <Hero />
         <News />
         {/* <About /> */}
+        <Sponsors />
       </Layout>
     )
   }
