0580531076e7c1ed20683b0ca29204930f9210ec
Willis <3475947+JasherIO@users.noreply.github.com>
2018-10-06 14:26:57 -0500

    Sponsor starter

---
+++ src/components/Home/Sponsors.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// See My Recent Work http://mattfarley.ca
+
+const Section = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        Sponsors go here
+      </div>
+    </section>
+  )
+}
+
+export default Section
