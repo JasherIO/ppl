7a7f48857fff7f5ef78a682dd0897a0dcad1837c
Willis <3475947+JasherIO@users.noreply.github.com>
2018-10-22 18:48:32 -0500

    Mock Moment.fromNow

--- src/pages/news/__tests__/index.test.js
+++ src/pages/news/__tests__/index.test.js
@@ -4,8 +4,16 @@ import { Title } from "../index"
 import data from './data.json'
 import NewsPage from '../index'
 
+jest.unmock('moment')
+const moment = jest.requireActual('moment')
+
 describe("NewsPage", () =>
   it("renders correctly", () => {
+    moment.prototype.fromNow = jest.fn()
+      .mockReturnValueOnce('4 days ago')
+      .mockReturnValueOnce('16 days ago')
+      .mockReturnValueOnce('19 days ago')
+
     const tree = renderer
       .create(<NewsPage data={data} />)
       .toJSON()
