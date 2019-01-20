0812b365c8e5bf1b738f42f159bd1fcb8eadcac8
Willis <3475947+JasherIO@users.noreply.github.com>
2019-01-20 06:08:45 -0600

    Personal name to gamertag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="footer">
     <div className="content has-text-centered">
       <p>
-        Developed by <a href="https://jasher.io">Josh Willis</a>. The <a href="https://github.com/JasherIO/ppl">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        Developed by <a href="https://jasher.io">Jasher</a>. The <a href="https://github.com/JasherIO/ppl">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
       </p>
     </div>
   </footer>
