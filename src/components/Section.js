665ab274760db46a564c497f247564215587ed1c
Willis <3475947+JasherIO@users.noreply.github.com>
2019-02-12 18:01:03 -0600

    Handle rest of props correctly

--- src/components/Section.js
+++ src/components/Section.js
@@ -12,10 +12,10 @@ const Section = (props) => {
 }
 
 export const Title = (props) => {
-  const { title, size } = props
+  const { title, size, ...rest } = props
   
   return (
-    <h1 className={`title is-${size}`}>{title}</h1>
+    <h1 className={`title is-${size}`} {...rest}>{title}</h1>
   )
 }
 
@@ -24,14 +24,14 @@ Title.defaultProps = {
 }
 
 export const Level = (props) => {
-  const { children, isMobile } = props
+  const { children, isMobile, ...rest } = props
   const styles = isMobile ? "level is-mobile" : "level"
 
   return (
     <div className={styles} style={{ marginBottom: "1rem" }}>
       <div className="level-left">
         <div className="level-item">
-          <Title {...props} />
+          <Title {...rest} />
         </div>
       </div>
       <div className="level-right">
